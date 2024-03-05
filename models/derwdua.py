87cc48e116b2d5ee883019ac54720db216facc70
amstroy <comepfc131421@163.com>
2024-03-05 19:26:33 +0800

    重放版本

---
+++ models/derwdua.py
@@ -0,0 +1,297 @@
+import logging
+import numpy as np
+from tqdm import tqdm
+import torch
+from torch import nn
+from torch import optim
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from models.base import BaseLearner
+from utils.inc_net import DERNet, IncrementalNet
+from utils.toolkit import count_parameters, target2onehot, tensor2numpy
+
+
+
+class DERWDUA(BaseLearner):
+    def __init__(self, args):
+        super().__init__(args)
+        self.args = args
+        self._network = DERNet(args["convnet_type"], False)
+        self.init_epoch = self.args["init_epoch"]
+        self.init_lr = self.args["init_lr"]
+        self.init_milestones = self.args["init_milestones"]
+        self.init_lr_decay = self.args["init_lr_decay"]
+        self.init_weight_decay = self.args["init_weight_decay"]
+
+        self.epochs = self.args["epochs"]
+        self.lrate = self.args["lrate"]
+        self.milestones = self.args["milestones"]
+        self.lrate_decay = self.args["lrate_decay"]
+        self.batch_size = self.args["batch_size"]
+        self.weight_decay = self.args["weight_decay"]
+        self.num_workers = self.args["num_workers"]
+
+
+    def after_task(self):
+        self._known_classes = self._total_classes
+        logging.info("Exemplar size: {}".format(self.exemplar_size))
+
+    def incremental_train(self, data_manager):
+        self._cur_task += 1
+        self._total_classes = self._known_classes + data_manager.get_task_size(
+            self._cur_task
+        )
+
+
+        if self.args['scenario'] == 'dcl':
+            self._total_classes = 6
+            self._known_classes = 0
+        else:
+            if self._cur_task != 0:
+                self._known_classes = self._known_classes - 5
+        self._network.update_fc(self._total_classes)
+        logging.info(
+            "Learning on {}-{}".format(self._known_classes, self._total_classes)
+        )
+        logging.info(
+            "domain:{} ".format(self.domain[self._cur_task])
+        )
+        if self._cur_task > 0:
+            for i in range(self._cur_task):
+                for p in self._network.convnets[i].parameters():
+                    p.requires_grad = False
+
+        logging.info("All params: {}".format(count_parameters(self._network)))
+        logging.info(
+            "Trainable params: {}".format(count_parameters(self._network, True))
+        )
+
+        train_dataset = data_manager.get_dataset(
+            np.arange(self._known_classes, self._total_classes),
+            source="train",
+            mode="train",
+            appendent=self._get_memory(),
+            domainTrans=self.domainTrans,
+            domain_type=self.domain[self._cur_task]
+        )
+        self.train_loader = DataLoader(
+            train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers
+        )
+        test_dataset = data_manager.get_dataset(
+            np.arange(0, self._total_classes), source="test", mode="test",
+            domainTrans=self.domainTrans,
+            domain_type=self.domain[self._cur_task]
+        )
+        self.test_loader = DataLoader(
+            test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers
+        )
+
+        if len(self._multiple_gpus) > 1:
+            self._network = nn.DataParallel(self._network, self._multiple_gpus)
+        self._train(self.train_loader, self.test_loader)
+        self.build_rehearsal_memory(data_manager, self.samples_per_class)
+        if len(self._multiple_gpus) > 1:
+            self._network = self._network.module
+
+    def train(self):
+        self._network.train()
+        if len(self._multiple_gpus) > 1 :
+            self._network_module_ptr = self._network.module
+        else:
+            self._network_module_ptr = self._network
+        self._network_module_ptr.convnets[-1].train()
+        if self._cur_task >= 1:
+            for i in range(self._cur_task):
+                self._network_module_ptr.convnets[i].eval()
+
+    def _train(self, train_loader, test_loader):
+        self._network.to(self._device)
+        if self._cur_task == 0:
+            optimizer = optim.SGD(
+                filter(lambda p: p.requires_grad, self._network.parameters()),
+                momentum=0.9,
+                lr=self.init_lr,
+                weight_decay=self.init_weight_decay,
+            )
+            scheduler = optim.lr_scheduler.MultiStepLR(
+                optimizer=optimizer, milestones=self.init_milestones, gamma=self.init_lr_decay
+            )
+            if not self.args['skip']:
+                self._init_train(train_loader, test_loader, optimizer, scheduler)
+            else:
+                test_acc = self._network.load_checkpoint(self.args)
+                cur_test_acc = self._compute_accuracy(self._network, test_loader)
+                logging.info(f"Loaded Test Acc:{test_acc} Cur_Test_Acc:{cur_test_acc}")
+                
+        else:
+            optimizer = optim.SGD(
+                filter(lambda p: p.requires_grad, self._network.parameters()),
+                lr=self.lrate,
+                momentum=0.9,
+                weight_decay=self.weight_decay,
+            )
+            scheduler = optim.lr_scheduler.MultiStepLR(
+                optimizer=optimizer, milestones=self.milestones, gamma=self.lrate_decay
+            )
+            self._update_representation(train_loader, test_loader, optimizer, scheduler)
+            if len(self._multiple_gpus) > 1:
+                self._network.module.weight_align(
+                    self._total_classes - self._known_classes
+                )
+            else:
+                self._network.weight_align(self._total_classes - self._known_classes  )
+
+    def _init_train(self, train_loader, test_loader, optimizer, scheduler):
+        prog_bar = tqdm(range(self.init_epoch))
+
+        for _, epoch in enumerate(prog_bar):
+            self.train()
+            losses = 0.0
+            correct, total = 0, 0
+            for i, (_, inputs, targets) in enumerate(train_loader):
+                inputs, targets = inputs.to(self._device), targets.to(self._device)
+
+
+                logits = self._network(inputs)["logits"]
+                # print("\n logits", logits[0])
+                loss = F.cross_entropy(logits, targets)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                losses += loss.item()
+
+                _, preds = torch.max(logits, dim=1)
+
+                correct += preds.eq(targets.expand_as(preds)).cpu().sum()
+
+                total += len(targets)
+
+
+            scheduler.step()
+            train_acc = np.around(tensor2numpy(correct) * 100 / total, decimals=2)
+
+            if epoch % 5 == 0:
+                test_acc = self._compute_accuracy(self._network, test_loader)
+                info = "Task {}, Epoch {}/{} => Loss {:.3f}, Train_accy {:.2f}, Test_accy {:.2f}".format(
+                    self._cur_task,
+                    epoch + 1,
+                    self.init_epoch,
+                    losses / len(train_loader),
+                    train_acc,
+                    test_acc,
+                )
+            else:
+                info = "Task {}, Epoch {}/{} => Loss {:.3f}, Train_accy {:.2f}".format(
+                    self._cur_task,
+                    epoch + 1,
+                    self.init_epoch,
+                    losses / len(train_loader),
+                    train_acc,
+                )
+            prog_bar.set_description(info)
+        # self._network.eval()
+        # mom_pre = 0.1
+        # for i, (_, inputs, targets) in enumerate(self.train_loader):
+        #     if i > 1:
+        #         break
+        #     inputs, targets = inputs.to(self._device), targets.to(self._device)
+        #     mom_new = (mom_pre * 0.94)
+        #     for m in self._network.modules():
+        #         # print(m)
+        #         if isinstance(m, torch.nn.modules.batchnorm._BatchNorm):
+        #             m.train()
+        #             m.momentum = mom_new + 0.005
+        #     mom_pre = mom_new
+        #
+        #     _ = self._network(inputs)
+
+        logging.info(info)
+
+    def _update_representation(self, train_loader, test_loader, optimizer, scheduler):
+        prog_bar = tqdm(range(self.epochs))
+
+        for _, epoch in enumerate(prog_bar):
+            self.train()
+            losses = 0.0
+            losses_clf = 0.0
+            losses_aux = 0.0
+            correct, total = 0, 0
+            for i, (_, inputs, targets) in enumerate(train_loader):
+                inputs, targets = inputs.to(self._device), targets.to(self._device)
+
+                outputs = self._network(inputs)
+                logits, aux_logits = outputs["logits"], outputs["aux_logits"]
+                loss_clf = F.cross_entropy(logits, targets)
+                aux_targets = targets.clone()
+
+                # print("---------aux_targets-------")
+                # print(aux_targets)
+                # print("\n---------self._known_classes-------")
+                # print(self._known_classes)
+                # print("---------logits-------")
+                # print(logits.shape)
+                aux_targets = torch.where(
+                    aux_targets - self._known_classes + 1 - 5 > 0,
+                    aux_targets - self._known_classes + 1 - 5,
+                    0,
+                )
+                # print("\n---------aux_targets-------")
+                # print(aux_targets.shape)
+                # print("\n---------aux_logits.shape-------")
+                # print(aux_logits.shape)
+                loss_aux = F.cross_entropy(aux_logits, aux_targets)
+                loss = loss_clf +loss_aux
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                losses += loss.item()
+                losses_aux += loss_aux.item()
+                losses_clf += loss_clf.item()
+
+                _, preds = torch.max(logits, dim=1)
+                correct += preds.eq(targets.expand_as(preds)).cpu().sum()
+                total += len(targets)
+
+
+            scheduler.step()
+            train_acc = np.around(tensor2numpy(correct) * 100 / total, decimals=2)
+            if epoch % 5 == 0:
+                test_acc = self._compute_accuracy(self._network, test_loader)
+                info = "Task {}, Epoch {}/{} => Loss {:.3f}, Loss_clf {:.3f}, Loss_aux {:.3f}, Train_accy {:.2f}, Test_accy {:.2f}".format(
+                    self._cur_task,
+                    epoch + 1,
+                    self.epochs,
+                    losses / len(train_loader),
+                    losses_clf / len(train_loader),
+                    losses_aux / len(train_loader),
+                    train_acc,
+                    test_acc,
+                )
+            else:
+                info = "Task {}, Epoch {}/{} => Loss {:.3f}, Loss_clf {:.3f}, Loss_aux {:.3f}, Train_accy {:.2f}".format(
+                    self._cur_task,
+                    epoch + 1,
+                    self.epochs,
+                    losses / len(train_loader),
+                    losses_clf / len(train_loader),
+                    losses_aux / len(train_loader),
+                    train_acc,
+                )
+            prog_bar.set_description(info)
+        # self._network.eval()
+        # mom_pre = 0.1
+        # for i, (_, inputs, targets) in enumerate(self.train_loader):
+        #     if i > 1:
+        #         break
+        #     inputs, targets = inputs.to(self._device), targets.to(self._device)
+        #     mom_new = (mom_pre * 0.94)
+        #     for m in self._network.modules():
+        #
+        #         if isinstance(m, torch.nn.modules.batchnorm._BatchNorm):
+        #             m.train()
+        #             m.momentum = mom_new + 0.005
+        #     mom_pre = mom_new
+        #
+        #     _ = self._network(inputs)
+        logging.info(info)
